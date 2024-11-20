a4ce20dd4968d0f02234edb590ec2b41392ef94a
halsan23 <halsan23@msn.com>
2024-11-20 12:06:52 -0700

    Made my Js ToDo List

---
+++ courses/cSteele-FSWD-Bootcamp-2024/courseProjects/toDoList/toDoList.js
@@ -0,0 +1,60 @@
+// JavaScript ToDo List
+
+// Initialize Vriables
+let toDoList = [];
+let choice = "";
+let addNew = "";
+let arrCheck = 0;
+
+while (choice !== "q" && choice !== "quit") {
+   choice = prompt("ToDo List: A)dd a New Item, L)ist Items, D)elete an Item, or  Q)uit?").toLowerCase();
+
+   // Add a new item
+   while (choice === "a" || choice === "add") {
+      addNew = prompt("Add a New Item or C)ancel?");
+      if (addNew.toLowerCase() !== "c" && addNew.toLowerCase() !== "cancel") {
+         toDoList.push(addNew);
+      } else {
+         choice = "";
+      }
+   }
+
+   // List Items
+   if (choice.toLowerCase() === "l" || choice.toLowerCase() === "list") {
+      if (toDoList.length < 1) {
+         console.log('No Items in ToDo List');
+         alert('ToDo List is Empty');
+      } else {
+         console.log('ToDo List');
+         console.log('===============');
+         for (i = 0; i < toDoList.length; i++) {
+            console.log(`ToDo #${i + 1}: ${toDoList[i]}`);
+         }
+         console.log('---------------');
+         choice = "";
+      }
+   }
+
+   // Delete Items
+   if (choice.toLowerCase() === "d" || choice.toLowerCase() === "delete") {
+      if (toDoList.length < 1) {
+         alert('The ToDo List is Empty : No Items to Delete!');
+         choice = "";
+      } else {
+         delItem = prompt("Delete an Existing ToDo Item or C)ancel?");
+         if (delItem.toLowerCase() !== "c" && delItem.toLowerCase() !== "cancel") {
+            for (i = 0; i < toDoList.length; i++) {
+               if (toDoList[i] === delItem) {
+                  toDoList.splice(i, 1);
+                  arrCheck = 1;
+               }
+               if (arrCheck === 0) {
+                  alert(`${delItem} not Found : Check spelling and try again:`)
+               }
+            }
+         }
+      }
+   }
+}
+
+console.log('All Done');
