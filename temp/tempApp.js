fa475ae2c4849bf4b620fbe0e47051faa7375629
halsan23 <halsan23@msn.com>
2024-11-21 11:41:04 -0700

    working on Js Functions cSteele

--- temp/tempApp.js
+++ temp/tempApp.js
@@ -1,25 +1,9 @@
-const scores = {
-    keenan: 80,
-    damon: 67,
-    kim: 89,
-    shawn: 91,
-    marlon: 72,
-    dwayne: 77,
-    nadia: 83,
-    elvira: 97,
-    diedra: 81,
-    vonnie:  60
+// Functions
+
+function getRandomNum(limit) {
+    console.log(Math.floor(Math.random() * limit) + 1);
 }
 
-for (let person in scores) {
-    console.log(`${person} scored a ${scores[person]}`);
-}
-
-console.log(Object.keys(scores));
-console.log(Object.values(scores));
-console.log(Object.entries(scores));
-
-
-for (let person of Object.keys(scores)) {
-    console.log(`${person} scored a ${scores[person]}`);
-}
+getRandomNum(5)
+getRandomNum(10)
+getRandomNum(20)
