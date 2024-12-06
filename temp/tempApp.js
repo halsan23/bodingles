410dc1aaee4d4fd18dbadaee5b772308b8d81adc
halsan23 <halsan23@msn.com>
2024-12-06 11:26:24 -0700

    ongoing Js Newer Features cSteele

--- temp/tempApp.js
+++ temp/tempApp.js
@@ -1,5 +1,10 @@
 // Functions
 
 // let randNum = getRandomNum => Math.floor(Math.random() * 6) + 1;
-let nums = [4, 7, 2, 9, 5, 1, 8];
-console.log(Math.max(...nums));
+
+const scores = [96840, 85465, 71496, 64824, 56982];
+
+const [high, second] = scores;
+
+console.log(high);
+console.log(second);
