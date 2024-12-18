8e6bbdb8da852215fd8cc349a28ee7821aa1a69f
halsan23 <halsan23@msn.com>
2024-12-18 12:11:56 -0700

    finished Js Properties getAttribute() and setAttribute() cSteele

--- temp/tempApp.js
+++ temp/tempApp.js
@@ -2,4 +2,12 @@
 
 // let randNum = getRandomNum => Math.floor(Math.random() * 6) + 1;
 
-document.querySelector('article').innerHTML = "The <i>Silkie</i> (sometimes spelled Silky) is a ...";
+
+// document.querySelectorAll('img')[2].setAttribute('src', "https://images.unsplash.com/photo-1581061090142-c2cd0ec9f021?w=200");
+
+// document.querySelectorAll('img')[2].src = 'https://images.unsplash.com/photo-1581061090142-c2cd0ec9f021?w=200';
+
+
+document.querySelectorAll('img')[2].src = "https://images.unsplash.com/photo-1581061090142-c2cd0ec9f021?w=200";
+
+document.querySelectorAll('img')[2].setAttribute('alt', "chicken");
