fbcd6a99a7738afd05b59b513f4ebac801c6f1d4
halsan23 <halsan23@msn.com>
2024-12-23 12:51:54 -0700

    finished Changing Styles with JavaScript cSteele

--- temp/tempApp.js
+++ temp/tempApp.js
@@ -8,6 +8,15 @@
 // document.querySelectorAll('img')[2].src = 'https://images.unsplash.com/photo-1581061090142-c2cd0ec9f021?w=200';
 
 
-document.querySelectorAll('img')[2].src = "https://images.unsplash.com/photo-1581061090142-c2cd0ec9f021?w=200";
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const letter = document.querySelectorAll('span');
+let count = 5;
 
-document.querySelectorAll('img')[2].setAttribute('alt', "chicken");
+console.log(colors);
+console.log(letter);
+
+for (let color of colors) {
+   letter[count].style.color = color;
+   console.log(count);
+   count++
+}
