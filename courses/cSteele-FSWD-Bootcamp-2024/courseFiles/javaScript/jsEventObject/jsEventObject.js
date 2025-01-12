71e8b5ba362882d89e77532159870398abda5ae5
halsan23 <halsan23@msn.com>
2025-01-12 15:38:42 -0700

    ongoing Event Objects and Key Trapping cSteele

--- courses/cSteele-FSWD-Bootcamp-2024/courseFiles/javaScript/jsEventObject/jsEventObject.js
+++ courses/cSteele-FSWD-Bootcamp-2024/courseFiles/javaScript/jsEventObject/jsEventObject.js
@@ -3,3 +3,16 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', function(evt) {
    console.log(evt);
 })
+
+
+const inp = document.querySelector('input');
+inp.addEventListener('keydown', function(evt) {
+   // console.log(evt);
+   console.log(evt.key);
+   console.log(evt.code);
+})
+
+
+window.addEventListener('keydown', function (e) {
+   console.log(e.code);
+})
