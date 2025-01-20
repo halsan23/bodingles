5d4a4e91d6f4751b5863688bdeb95238edc0bfb8
halsan23 <halsan23@msn.com>
2025-01-20 12:08:48 -0700

    ongoing PingPong Scorekeeper courseProject cSteele

--- courses/cSteele-FSWD-Bootcamp-2024/courseProjects/scoreKeeper/scoreKeeper.js
+++ courses/cSteele-FSWD-Bootcamp-2024/courseProjects/scoreKeeper/scoreKeeper.js
@@ -4,45 +4,59 @@
 // ===============================================
 
 // Define the variables
-let leftNum = document.querySelector('#leftNum');
-let rightNum = document.querySelector('#rightNum');
+let leftNum = document.querySelector('#leftNum'); // Left Score Number
+let rightNum = document.querySelector('#rightNum'); // Right Score Number
+const numbSel = document.getElementById('numbSel'); // Number of Rounds Select
 
+const buttonOne = document.querySelector('#buttonOne'); // Player 1 Button
+const buttonTwo = document.querySelector('#buttonTwo'); // Player 2 Button
+const reset = document.querySelector('#buttonReset'); // Reset Button
 
-
-let numbSel = document.getElementsByName('#numbSel');
-console.log(`number select = ${numbSel.value}`);
-
-
-
-const buttonOne = document.querySelector('#buttonOne');
-const buttonTwo = document.querySelector('#buttonTwo');
-const reset = document.querySelector('#buttonReset');
-
+// Win/Loss Space to Announce Winner
+let winLose = document.getElementById('winLoss');
+let resetDisp = document.getElementById('resetAnnounce');
+winLose.append('');
+resetDisp.append('');
 
 // initial setup
 let numOne = 0;
 let numTwo = 0;
-let numSel = 3;
+let numbSelVal = 3;
 
 leftNum.innerText = numOne;
 rightNum.innerText = numTwo;
-numbSel.innerText.value = numSel;
+numbSel.value = numbSelVal;
+console.log(`number select value = ${numbSelVal}`);
 
 
-// buttonOne click event
+
+// CLICK EVENTS
+// Click Event to set Number of Rounds
+document.getElementById('numbSel').addEventListener('click', function(evt) {
+   numbSelVal = document.getElementById('numbSel').value;
+   console.log(`number select value = ${numbSelVal}`);
+})
+
+
+
+// Player One Button Click Event
 buttonOne.addEventListener('click', function(evt) {
-   console.log('Left Number Button Clicked');
-   console.log(evt);
+   console.log('Player One Button Clicked');
+   numOne++;
+   leftNum.innerText = numOne;
+   if (numOne === numbSelVal) {
+      winLose.append('Player One Wins!')
+      resetDisp.append('Press Reset to Play Again');
+   }
 })
 
-// buttonTwo click event
+// Player Two Button Click Event
 buttonTwo.addEventListener('click', function() {
-   console.log('Right Number Button Clicked');
-   console.log(evt);
+   console.log('Player Two Button Clicked');
 })
 
-// Reset click event
+// Reset Button Click Event
 reset.addEventListener('click', function() {
    console.log('Reset Button Clicked');
-   console.log(evt);
 })
+
