28a420be280b6d83ff41cad73731df7c9964854d
halsan23 <halsan23@msn.com>
2025-02-08 17:10:15 -0700

    dd

--- courses/cSteele-FSWD-Bootcamp-2024/courseFiles/ajax/fetch/fetch.js
+++ courses/cSteele-FSWD-Bootcamp-2024/courseFiles/ajax/fetch/fetch.js
@@ -1,40 +1,24 @@
-// fetch("https://swapi.tech/api/people/1/")
-//   .then((res) => {
-//     console.log("RESOLVED!", res);
-//     return res.json();
-//   })
-//   .then((data) => {
-//     console.log(data.result.properties);
-//   })
-//   .catch((e) => {
-//     console.log("ERROR!", e);
-//   });
+// simple fetch() to return a single request
+fetch("https://swapi.tech/api/people/1/")
+  .then((res) => {
+    console.log("Person 1:", res);
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data.result.properties);
+  })
+  .catch((e) => {
+    console.log("ERROR!", e);
+  });
 
-// fetch("https://swapi.tech/api/people/1/")
-//   .then((res) => {
-//     console.log("RESOLVED!", res);
-//     return res.json();
-//   })
-//   .then((data) => {
-//     console.log(data.result.properties);
-//     return fetch("https://swapi.tech/api/people/2/");
-//   })
-//   .then((res) => {
-//     console.log("SECOND REQUEST RESOLVED!!!");
-//     return res.json();
-//   })
-//   .then((data) => {
-//     console.log(data.result.properties);
-//   })
-//   .catch((e) => {
-//     console.log("ERROR!", e);
-//   });
 
+// Async function to send multiple requests
 const loadStarWarsPeople = async (id) => {
    let apiAddress = `https://swapi.tech/api/people/${id}/`;
    try {
       const res = await fetch(apiAddress);
       const data = await res.json();
+      console.log(`Person ${id}:`)
       console.log(data.result.properties);
    } catch (e) {
       console.log("ERROR!!!", e);
