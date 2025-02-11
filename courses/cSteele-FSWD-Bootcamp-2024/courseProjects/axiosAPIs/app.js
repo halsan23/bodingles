715ea79243393f9f718dfe499e74395345690881
halsan23 <halsan23@msn.com>
2025-02-10 20:58:09 -0700

    ongoing Axios APIs cSteele | badDoggy

--- courses/cSteele-FSWD-Bootcamp-2024/courseProjects/axiosAPIs/app.js
+++ courses/cSteele-FSWD-Bootcamp-2024/courseProjects/axiosAPIs/app.js
@@ -26,22 +26,33 @@ button.addEventListener("click", addNewJoke);
 // Star Wars API
 // =====================================================================
 // set variable to print names
-let starWars = document.getElementById('starWars');
+let swNames = document.getElementById('swNames');
 
 // Using Axios and a Async function to send multiple requests
 const getStarWarsPeople = async () => {
-  try {
-    const res = await axios.get(`https://swapi.tech/api/people/`);
-    console.log(res.data.results);
-    let people =  res.data.results;
-    people.forEach(item => {
-      let newP = document.createElement('P');
-      newP.innerText = item.name;
-      starWars.append(newP);
-    })
-  } catch (e) {
-    starWars.innerText = `ERROR!!!, ${e}`;
-  }
+    try {
+        // axios method
+        // -----------------------------------------------------------------
+        const res = await axios.get(`https://swapi.tech/api/people/`);
+        let people =  res.data.results;
+        // -----------------------------------------------------------------
+
+        // fetch method
+        // -----------------------------------------------------------------
+        // const res = await fetch(`https://swapi.tech/api/people/`);
+        // const resData = await res.json();
+        // let people = resData.results;
+        // -----------------------------------------------------------------
+
+        // display EACH item from people
+        people.forEach(item => {
+            let newOption = document.createElement('OPTION');
+            newOption.innerText = item.name;
+            swNames.append(newOption);
+      })
+    } catch (e) {
+        swNames.innerText = `ERROR!!!, ${e}`;
+    }
 };
 
 getStarWarsPeople();
