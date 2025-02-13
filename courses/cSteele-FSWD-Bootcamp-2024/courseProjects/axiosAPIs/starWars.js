cc82f2e7cce7c6e9bdadb4990658bb240e5c18e0
halsan23 <halsan23@msn.com>
2025-02-12 17:49:45 -0700

    ongoing Star Wars API badDoggy

--- courses/cSteele-FSWD-Bootcamp-2024/courseProjects/axiosAPIs/starWars.js
+++ courses/cSteele-FSWD-Bootcamp-2024/courseProjects/axiosAPIs/starWars.js
@@ -1,49 +1,50 @@
-// Star Wars API
-// =====================================================================
-// set variable for html place holders
-const swNames = document.getElementById('swNames');
-// const swPlanets = document.getElementById('swPlanets');
-// const swShips = document.getElementById('swShips');
-const disp = document.getElementById('disp');
-// set variables for api processing
-let placeHolder = '';
+
+const categories = document.getElementById('categories');
+const specific = document.getElementById('specific');
+const display = document.getElementById('display');
+let htmlPlace = '';
 let lookFor = '';
 
-
-// Using Axios and a Async function to send multiple requests
-// Get Star Wars data from API
-const getStarWarsData = async (placeHolder, lookFor) => {
+const dispCategs = async (htmlPlace) => {
     try {
-        // get api data
-        // -----------------------------------------------------------------
-        const res = await axios.get(`https://swapi.tech/api/${lookFor}/`);
-        let results =  res.data.results;
-        // -----------------------------------------------------------------
+        const res = await axios.get(`https://swapi.tech/api/`);
+        const result = Object.keys(res.data.result);
 
-        for (let result of results) {
-            let newLI = document.createElement('LI');
-            newLI.innerText = result.name;
-            placeHolder.append(newLI);
+        for (let item of result) {
+            if (item != 'films') {
+                let newLI = document.createElement('LI');
+                newLI.innerText = item.charAt(0).toUpperCase() + item.slice(1);
+                htmlPlace.append(newLI);
+            }
         }
 
-        const person = displayData(lookFor, 4);
-        console.log(`Person is: ${person.properties}`);
-        // let newLI2 = document.createElement('LI');
-        // newLI2.innerText = ``;
-        // disp.append(newLI2);
+        categories.addEventListener('click', function(event) {
+            if(event.target.tagName === 'LI') {
+                dispSelect(event.target.textContent);
+            }
+        });
     } catch (e) {
-        placeHolder.innerText = `ERROR!!!, ${e}`;
+        console.log(`ERROR!!!, ${e}`);
     }
-};
-
-
-const displayData = async (lookFor, id) => {
-    const res = await axios.get(`https://swapi.tech/api/${lookFor}/${id}/`);
-    console.log(res.data.result.properties);
-    return res.data.result;
 }
 
 
+const dispSelect = async (item) => {
+    try {
+        const res = await axios.get(`https://swapi.tech/api/${item}/`);
+        const result2 = Object.keys(res.data.results);
+
+        for (let i = 1; i < result2.length + 1; i++) {
+            const itemSel = await axios.get(`https://swapi.tech/api/${item}/${i}/`);
+            let selName = itemSel.data.result.properties.name;
+            let newLI = document.createElement('LI');
+            newLI.innerText = selName.charAt(0).toUpperCase() + selName.slice(1);
+            specific.append(newLI);
+        }
+    } catch (e) {
+        console.log(`ERROR!!!, ${e}`);
+    }
+}
+
 // run each function
-lookFor = 'people';
-getStarWarsData(swNames, lookFor);
+dispCategs(categories);
