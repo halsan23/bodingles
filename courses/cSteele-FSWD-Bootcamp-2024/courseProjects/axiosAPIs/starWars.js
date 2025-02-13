dea9a2e304f4123adfc2de7e96031f13809e09f3
halsan23 <halsan23@msn.com>
2025-02-13 15:18:06 -0700

    ongoing Star Wars API badDoggy

--- courses/cSteele-FSWD-Bootcamp-2024/courseProjects/axiosAPIs/starWars.js
+++ courses/cSteele-FSWD-Bootcamp-2024/courseProjects/axiosAPIs/starWars.js
@@ -1,24 +1,36 @@
+// Star Wars API
+
+// Set Base Variables
 const categories = document.getElementById('categories');
+const selHeader = document.getElementById('selHeader');
+const dispHeader = document.getElementById('dispHeader');
 const specific = document.getElementById('specific');
 const display = document.getElementById('display');
 
+// Display Main Categories
 const dispCategs = async (category) => {
     try {
+        // get category names
         const res = await axios.get(`https://swapi.dev/api/`);
         const result = Object.keys(res.data);
 
+        // create new list items (exclude films, vehicles, & starships)
         for (let item of result) {
-            let newLI = document.createElement('LI');
-            newLI.innerText = item.charAt(0).toUpperCase() + item.slice(1);
-            category.append(newLI);
+            if (item != 'films' && item != 'vehicles' && item != 'starships') {
+                let newLI = document.createElement('LI');
+                newLI.innerText = item.charAt(0).toUpperCase() + item.slice(1);
+                category.append(newLI);
+            }
         }
 
+        // add click events for menu LI's
         categories.addEventListener('click', function(event) {
-            if(event.target.tagName === 'LI') {
+            if (event.target.tagName === 'LI') {
+                // delete LI's & display new data
                 specific.innerHTML = '';
                 let text = event.target.textContent
                 selectedItem = text.charAt(0).toLowerCase() + text.slice(1);
-                dispSelect(selectedItem);
+                genDisplay(selectedItem);
             }
         });
     } catch (e) {
@@ -26,9 +38,17 @@ const dispCategs = async (category) => {
     }
 }
 
-
-const dispSelect = async (item) => {
+// Display Specific Categories
+const genDisplay = async (item) => {
     try {
+        // delete any existing header & add new header info
+        selHeader.innerHTML = '';
+        let itemText = item.charAt(0).toUpperCase() + item.slice(1);
+        let newLI = document.createElement('LI');
+        newLI.innerHTML = `<span class="firstWord">${itemText}</span><br><hr>`;
+        selHeader.append(newLI);
+
+        // create and display specific items
         const res = await axios.get(`https://swapi.dev/api/${item}/`);
         const results = res.data.results;
 
@@ -44,5 +64,6 @@ const dispSelect = async (item) => {
     }
 }
 
-// run each function
+
+// display the Main Categories
 dispCategs(categories);
