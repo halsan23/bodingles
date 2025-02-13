dc8fcf2fb6d9d0d59403ae32627165bbc5425252
halsan23 <halsan23@msn.com>
2025-02-13 11:50:35 -0700

    ongoing Star Wars Trivia badDoggy

--- courses/cSteele-FSWD-Bootcamp-2024/courseProjects/axiosAPIs/starWars.js
+++ courses/cSteele-FSWD-Bootcamp-2024/courseProjects/axiosAPIs/starWars.js
@@ -7,20 +7,21 @@ let lookFor = '';
 
 const dispCategs = async (htmlPlace) => {
     try {
-        const res = await axios.get(`https://swapi.tech/api/`);
-        const result = Object.keys(res.data.result);
+        const res = await axios.get(`https://swapi.dev/api/`);
+        const result = Object.keys(res.data);
 
         for (let item of result) {
-            if (item != 'films') {
-                let newLI = document.createElement('LI');
-                newLI.innerText = item.charAt(0).toUpperCase() + item.slice(1);
-                htmlPlace.append(newLI);
-            }
+            let newLI = document.createElement('LI');
+            newLI.innerText = item.charAt(0).toUpperCase() + item.slice(1);
+            htmlPlace.append(newLI);
         }
 
         categories.addEventListener('click', function(event) {
             if(event.target.tagName === 'LI') {
-                dispSelect(event.target.textContent);
+                specific.innerHTML = '';
+                let text = event.target.textContent
+                selectedItem = text.charAt(0).toLowerCase() + text.slice(1);
+                dispSelect(selectedItem);
             }
         });
     } catch (e) {
@@ -31,12 +32,12 @@ const dispCategs = async (htmlPlace) => {
 
 const dispSelect = async (item) => {
     try {
-        const res = await axios.get(`https://swapi.tech/api/${item}/`);
-        const result2 = Object.keys(res.data.results);
+        const res = await axios.get(`https://swapi.dev/api/${item}/`);
+        const results = res.data.results;
 
-        for (let i = 1; i < result2.length + 1; i++) {
-            const itemSel = await axios.get(`https://swapi.tech/api/${item}/${i}/`);
-            let selName = itemSel.data.result.properties.name;
+        for (let i = 1; i < results.length + 1; i++) {
+            const itemSel = await axios.get(`https://swapi.dev/api/${item}/${i}/`);
+            let selName = itemSel.data.name;
             let newLI = document.createElement('LI');
             newLI.innerText = selName.charAt(0).toUpperCase() + selName.slice(1);
             specific.append(newLI);
