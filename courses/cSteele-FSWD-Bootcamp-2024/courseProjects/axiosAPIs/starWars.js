07e91599f4dc9b2146c9062111573591858c3140
halsan23 <halsan23@msn.com>
2025-02-14 16:01:53 -0700

    dd

--- courses/cSteele-FSWD-Bootcamp-2024/courseProjects/axiosAPIs/starWars.js
+++ courses/cSteele-FSWD-Bootcamp-2024/courseProjects/axiosAPIs/starWars.js
@@ -2,11 +2,10 @@
 
 // Set Base Variables
 const instructions = document.getElementById('instructions');
-const butt = document.getElementById('butt');
 const categories = document.getElementById('categories');
 const selHeader = document.getElementById('selHeader');
-const dispHeader = document.getElementById('dispHeader');
 const specific = document.getElementById('specific');
+const dispHeader = document.getElementById('dispHeader');
 const display = document.getElementById('display');
 
 // Display Main Categories
@@ -41,24 +40,24 @@ const dispCategs = async (category) => {
 }
 
 // Display Specific Categories
-const selectDisplay = async (item) => {
+const selectDisplay = async (selectedItem) => {
     try {
-        let itemText = item.charAt(0).toUpperCase() + item.slice(1);
+        let selectedCat = selectedItem.charAt(0).toUpperCase() + selectedItem.slice(1);
 
         // Change Instructions Message
-        instructions.innerText = `Select one of the Star Wars ${itemText} for specific details`;
+        instructions.innerText = `Select one of the Star Wars ${selectedCat} for specific details`;
+
         // delete any existing header & add new header info
-        selHeader.innerHTML = '';
         let newLI = document.createElement('LI');
-        newLI.innerHTML = `<h6>${itemText}</h6><hr>`;
+        newLI.innerHTML = `<h6>${selectedCat}</h6><hr>`;
         selHeader.append(newLI);
 
         // create and display specific items
-        const res = await axios.get(`https://swapi.dev/api/${item}/`);
-        const results = res.data.results;
+        let res = await axios.get(`https://swapi.dev/api/${selectedItem}/`);
+        let results = res.data.results;
 
         for (let i = 1; i < results.length + 1; i++) {
-            const itemSel = await axios.get(`https://swapi.dev/api/${item}/${i}/`);
+            const itemSel = await axios.get(`https://swapi.dev/api/${selectedItem}/${i}/`);
             let selName = itemSel.data.name;
             let newLI = document.createElement('LI');
             newLI.innerText = selName.charAt(0).toUpperCase() + selName.slice(1);
@@ -69,17 +68,6 @@ const selectDisplay = async (item) => {
     }
 }
 
-const reset = () => {
-    instructions.innerText = 'Select a Main Category Item to begin';
-    categories.innerHTML = '';
-    specific.innerHTML = '';
-    dispCategs(categories);
-    console.log(specific);
-}
-
-butt.addEventListener('click', function() {
-    reset();
-})
 
 // Initial Set
 instructions.innerText = 'Select a Main Category Item to begin';
