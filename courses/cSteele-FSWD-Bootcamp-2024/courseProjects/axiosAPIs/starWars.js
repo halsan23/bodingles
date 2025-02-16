14a7ff2027eb65c5fbb5f202782caed80be9abd2
halsan23 <halsan23@msn.com>
2025-02-16 16:28:21 -0700

    finally solved problems with Star Wars API display - need to finish details output and comment the code badDoggy

--- courses/cSteele-FSWD-Bootcamp-2024/courseProjects/axiosAPIs/starWars.js
+++ courses/cSteele-FSWD-Bootcamp-2024/courseProjects/axiosAPIs/starWars.js
@@ -6,9 +6,9 @@ const selHeader = document.getElementById('selHeader');
 const selList = document.getElementById('selList');
 const dispHeader = document.getElementById('dispHeader');
 const dispItem = document.getElementById('dispItem');
-let output = [];
 
 async function dispCategs() {
+    instructions.innerText = 'Select a Main Category Item to begin:';
     try {
         let res = await axios.get(`https://swapi.dev/api/`);
         let result = Object.keys(res.data);
@@ -16,17 +16,19 @@ async function dispCategs() {
         for (let item of result) {
             if (item != 'films' && item != 'vehicles' && item != 'starships') {
                 let newLI = document.createElement('LI');
-                newLI.innerText = item;
+                let listItem = firstLetterUp(item);
+                newLI.innerText = listItem;
                 categories.append(newLI);
             }
         }
 
-        categories.addEventListener('click', function (event) {
+        categories.onclick = (event) => {
             if (event.target.tagName === 'LI') {
-                let category = event.target.textContent;
+                let cat = event.target.textContent;
+                let category = firstLetterDn(cat)
                 selectDisplay(category);
             }
-        });
+        };
     } catch (e) {
         console.log(`ERROR!!!, ${e}`);
     }
@@ -34,7 +36,18 @@ async function dispCategs() {
 
 // Display Specific Categories
 const selectDisplay = async (category) => {
+    selHeader.innerHTML = '';
     selList.innerHTML = '';
+    dispHeader.innerHTML = '';
+    dispItem.innerHTML = '';
+    let capCat = firstLetterUp(category);
+
+    instructions.innerText = `Select one of the Star Wars ${capCat} for details:`;
+
+    let newLI = document.createElement('LI');
+    newLI.innerHTML = `<h6>${capCat}</h6><hr>`
+    selHeader.append(newLI)
+
     try {
         let res2 = await axios.get(`https://swapi.dev/api/${category}/`);
         let result2 = res2.data.results;
@@ -47,37 +60,45 @@ const selectDisplay = async (category) => {
             selList.append(newLI);
         }
 
-        output = [];
-        output[0] = category;
-        category = '';
-
-        selList.addEventListener('click', function(event) {
+        selList.onclick = (event) => {
             if (event.target.tagName === 'LI') {
-                output[1] = '';
                 selectedItem = event.target.textContent
-                displayDetails(selectedItem);
+                displayDetails(category, selectedItem);
             }
-        });
+        };
     } catch (e) {
         console.log(`ERROR!!!, ${e}`);
     }
 }
 
-const displayDetails = async (selectedItem) => {
-    output[1] = selectedItem;
-    console.log(`Cat: ${output[0]}`)
-    console.log(`Sel: ${output[1]}`)
-    console.log(`Output: ${output}`)
-    output = [];
-
+const displayDetails = async (category, selectedItem) => {
+    dispHeader.innerHTML = '';
     dispItem.innerHTML = '';
+    let capItem = firstLetterUp(selectedItem);
 
-    // const detailSel = await axios.get(`https://swapi.dev/api/${data[0]}/?search=${data[1]}`);
+    instructions.innerText = `Details for ${capItem}:`;
 
-    // output = detailSel.data.results[0];
-    // dispHeader.append(newLI);
+    let newLI = document.createElement('LI');
+    newLI.innerHTML = `<h6>${capItem}</h6><hr>`;
+    dispHeader.append(newLI);
+
+    const detailSel = await axios.get(`https://swapi.dev/api/${category}/?search=${selectedItem}`);
+    let output = detailSel.data.results[0];
+
+    let newLI2 = document.createElement('LI');
+    newLI2.innerText = `${capItem} Details`;
+    dispItem.append(newLI2);
+}
+
+// function to capitalize
+function firstLetterUp(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// function to LowerCase
+function firstLetterDn(string) {
+    return string.charAt(0).toLowerCase() + string.slice(1);
 }
 
 // Initial Set
-    instructions.innerText = 'Select a Main Category Item to begin';
-    dispCategs();
+dispCategs();
