7b5d30ae255373855205f48caf88cb92f3149a1b
halsan23 <halsan23@msn.com>
2025-02-17 11:49:01 -0700

    Ongoing Star Wars API needs final troubleshooting badDoggy

--- courses/cSteele-FSWD-Bootcamp-2024/courseProjects/axiosAPIs/starWars.js
+++ courses/cSteele-FSWD-Bootcamp-2024/courseProjects/axiosAPIs/starWars.js
@@ -84,13 +84,74 @@ const displayDetails = async (category, selectedItem) => {
 
     const detailSel = await axios.get(`https://swapi.dev/api/${category}/?search=${selectedItem}`);
     let output = detailSel.data.results[0];
-
-    let newLI2 = document.createElement('LI');
-    newLI2.innerText = `${capItem} Details`;
-    dispItem.append(newLI2);
+    if (category === 'people') {
+        person(output);
+    }
+    if (category === 'planets') {
+        planet(output);
+    }
+    if (category === 'species') {
+        species(output)
+    }
 }
 
-// function to capitalize
+const person = async (data) => {
+    let personData = [];
+    if (data.species.length == 0) {
+        personData.push('Species: Human');
+    } else {
+        const res = await axios.get(data.species[0]);
+        let speciesType = res.data.name;
+        personData.push(`Species: ${speciesType}`);
+    }
+    personData.push(`Gender: ${firstLetterUp(data.gender)}`);
+    personData.push(`Birthyear: ${data.birth_year}`);
+    personData.push(`Height: ${data.height}cm`);
+    personData.push(`Weight: ${data.mass}kg`);
+    personData.push(`Skin Color: ${firstLetterUp(data.skin_color)}`);
+    personData.push(`Hair Color: ${firstLetterUp(data.hair_color)}`);
+    personData.push(`Eye Color: ${firstLetterUp(data.eye_color)}`);
+    const res = await axios.get(data.homeworld);
+    let home = res.data.name;
+    personData.push(`Homeworld: ${home}`);
+    for (let i = 0; i < personData.length; i++) {
+        let newLI2 = document.createElement('LI');
+        newLI2.innerText = personData[i];
+        dispItem.append(newLI2);
+    }
+}
+
+const planet = async (data) => {
+    let planetData = [];
+    planetData.push(`Diameter: ${data.diameter}km`);
+    planetData.push(`Gravity: ${data.gravity} Gs`);
+    planetData.push(`Orbital Period: ${data.orbital_period} Earth Days`);
+    planetData.push(`Rotational Period: ${data.rotation_period} Earth Hours`);
+    planetData.push(`Climate: ${firstLetterUp(data.climate)}`);
+    planetData.push(`Surface Water: ${data.surface_water}%`);
+    planetData.push(`Terrain: ${firstLetterUp(data.terrain)}`);
+    planetData.push(`Average Population: ${data.population}`);
+    for (let i = 0; i < planetData.length; i++) {
+        let newLI2 = document.createElement('LI');
+        newLI2.innerText = planetData[i];
+        dispItem.append(newLI2);
+    }
+}
+
+const species = async (data) => {
+    let speciesData = [];
+    speciesData.push(`Designation: ${data.designation}`);
+    speciesData.push(`Average Lifespan: ${data.average_lifespan} Earth Years`);
+    speciesData.push(`Average Height: ${data.average_height}cm`);
+    speciesData.push(`Language: ${data.language}`);
+    for (let i = 0; i < speciesData.length; i++) {
+        let newLI2 = document.createElement('LI');
+        newLI2.innerText = speciesData[i];
+        dispItem.append(newLI2);
+    }
+}
+
+// function to Capitalize
 function firstLetterUp(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
