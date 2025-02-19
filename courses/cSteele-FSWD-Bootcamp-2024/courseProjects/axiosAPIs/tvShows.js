d973e854b0812ac61c78f56acf51e7fe6f4e1b04
halsan23 <halsan23@msn.com>
2025-02-18 17:51:42 -0700

    Finished TV Shows API badDoggy

--- courses/cSteele-FSWD-Bootcamp-2024/courseProjects/axiosAPIs/tvShows.js
+++ courses/cSteele-FSWD-Bootcamp-2024/courseProjects/axiosAPIs/tvShows.js
@@ -5,6 +5,7 @@
 const form = document.querySelector('#searchForm');
 const showInput = document.querySelector('#showInput');
 const tvShowDisplay = document.querySelector('#tvShowDisplay');
+const errorDisp = document.querySelector('#errorDisp');
 document.getElementsByName('showInput')[0].placeholder = 'TV Show';
 
 
@@ -18,32 +19,66 @@ form.addEventListener('submit', async function (evt) {
    // specifically the "elements.query.value" properties
    const searchTerm = form.elements.showInput.value;
 
+   // clear text input field on submit
+   // reset text input placeholder
    document.getElementsByName('showInput')[0].value = '';
    document.getElementsByName('showInput')[0].placeholder = 'TV Show';
    tvShowDisplay.innerText = '';
 
-   // send request for TV Show data
-   const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
+   try {
+      // send request for TV Show data
+      const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
 
-   // display the results
-   displayImages(res.data)
+      // display the results
+      displayShows(res.data);
+
+      } catch (evt) {
+         errorDisp.innerHTML = `<span class="error">${evt}</span>`;
+         console.log(evt);
+   }
 })
 
 
 // create and display the new image
-const displayImages = (shows) => {
+const displayShows = (shows) => {
+
    // count through each show
    for (let item of shows) {
+      thisShow = item.show;
+      console.log(thisShow);
+
+      let divParent = document.createElement('DIV');
+      divParent.className = "divParent";
+      let divImage = document.createElement('DIV');
+      divImage.className = "divImage";
+      let divText = document.createElement('DIV');
+      divText.className = "divText";
+
       // if database has no img to display
-      if (!item.show.image) {
-         let showName = document.createElement('SPAN');
-         showName.innerHTML = `<p><b>${item.show.name}</b></p><p>No Image<br>Available</p>`;
-         tvShowDisplay.append(showName);
+      if (!thisShow.image) {
+         divImage.innerHTML = `<p>No Image<br>Available</p>`;
+
       // if database does have an img to display
       } else {
          const img = document.createElement('IMG');
-         img.src = item.show.image.medium;
-         tvShowDisplay.append(img);
+         img.src = thisShow.image.medium;
+         divImage.append(img);
       }
+      divParent.append(divImage);
+
+      let prating = document.createElement('P');
+      prating.innerText = `Average Rating: ${thisShow.rating.average}`;
+      divText.append(prating);
+
+      pPrem = document.createElement('P');
+      pPrem.innerText = `Premiered: ${thisShow.premiered}`;
+      divText.append(pPrem);
+
+      pEnd = document.createElement('P');
+      pEnd.innerText = `Ended: ${thisShow.ended}`;
+      divText.append(pEnd);
+
+      divParent.append(divText);
+      tvShowDisplay.append(divParent);
    }
 }
