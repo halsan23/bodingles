3f9f2ba324b0cbd09752f85bd6c7f951eb4b2180
halsan23 <halsan23@msn.com>
2025-02-20 11:48:37 -0700

    Updating Header/Footer to be separate, external Js files - process will take some time badDoggy

---
+++ footer.js
@@ -0,0 +1,10 @@
+let appfooter = `
+    <nav>
+      <div>Website created by badDoggy</div>
+      <div>|</div>
+      <div>2024</div>
+      <div>|</div>
+      <div>&copy; All Rights Reserved</div>
+    </nav>
+`;
+document.getElementById("app-footer").innerHTML = appfooter;
---
+++ header.js
@@ -0,0 +1,14 @@
+let appHeader = `
+    <nav>
+      <span class="headLogo"></span>
+      <span class="headTitle">Bo</span>
+      <span class="headTitleb">dingles<sub>TM</sub></span>
+      <span class="headScroll"><img src="/images/scroll.png" alt=""></span>
+      <span class="headSlogan">
+        - -&nbsp;&nbsp;&nbsp;Bright
+        <a href="/temp/temp.html"><img class="sloganBulb" src="/images/logos/lightbulb.png" alt="Lightbulb Graphic"></a>
+         Ideas&nbsp;&nbsp;&nbsp;- -
+      </span>
+    </nav>
+`;
+document.getElementById("app-header").innerHTML = appHeader;
