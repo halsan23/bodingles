5b14cf5efb6f7f0de4f7748508221e9be797f560
halsan23 <halsan23@msn.com>
2025-02-20 15:38:42 -0700

    dd

--- header.js
+++ header.js
@@ -4,9 +4,9 @@ let rootPath = '';
 
 const getRoot = () => {
   if (window.location.hostname === 'halsan23.github.io') {
-    return 'halsan23.github.io/bodingles/';
+    return 'halsan23.github.io/bodingles';
   } else {
-    return './';
+    return '.';
   }
 }
 
