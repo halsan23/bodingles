45c88f07da6ad7e43b9618ad3356109bac4e0f5e
halsan23 <halsan23@msn.com>
2025-02-20 15:24:33 -0700

    dd

--- header.js
+++ header.js
@@ -1,18 +1,3 @@
-let appHeader = `
-    <nav>
-      <span class="headLogo"></span>
-      <span class="headTitle">Bo</span>
-      <span class="headTitleb">dingles<sub>TM</sub></span>
-      <span class="headScroll"><img src="/images/scroll.png" alt=""></span>
-      <span class="headSlogan">
-        - -&nbsp;&nbsp;&nbsp;Bright
-        <a href="/temp/temp.html"><img class="sloganBulb" src="/images/logos/lightbulb.png" alt="Lightbulb Graphic"></a>
-         Ideas&nbsp;&nbsp;&nbsp;- -
-      </span>
-    </nav>
-`;
-document.getElementById("app-header").innerHTML = appHeader;
-
 var root = window.location.hostname;
 console.log(`root = ${root}`);
 
@@ -22,5 +7,22 @@ if (root === 'https://halsan23.github.io') {
   rootPath = './';
 }
 
-
 console.log(`rootPath = ${rootPath}`);
+
+
+
+let appHeader = `
+    <nav>
+      <span class="headLogo"></span>
+      <span class="headTitle">Bo</span>
+      <span class="headTitleb">dingles<sub>TM</sub></span>
+      <span class="headScroll"><img src="${rootPath}/images/scroll.png" alt=""></span>
+      <span class="headSlogan">
+        - -&nbsp;&nbsp;&nbsp;Bright
+        <a href="/temp/temp.html"><img class="sloganBulb" src="${rootPath}/images/logos/lightbulb.png" alt="Lightbulb Graphic"></a>
+         Ideas&nbsp;&nbsp;&nbsp;- -
+      </span>
+    </nav>
+`;
+
+document.getElementById("app-header").innerHTML = appHeader;
