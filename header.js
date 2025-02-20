2642b8b8e3b9412f62c1fdc54d391f103ddbf479
halsan23 <halsan23@msn.com>
2025-02-20 16:20:01 -0700

    dd

--- header.js
+++ header.js
@@ -21,10 +21,10 @@ let appHeader = `
       <span class="headLogo"></span>
       <span class="headTitle">Bo</span>
       <span class="headTitleb">dingles<sub>TM</sub></span>
-      <span class="headScroll"><img src="/images/scroll.png" alt=""></span>
+      <span class="headScroll"><img src="./images/scroll.png" alt=""></span>
       <span class="headSlogan">
         - -&nbsp;&nbsp;&nbsp;Bright
-        <a href="/temp/temp.html"><img class="sloganBulb" src="/images/logos/lightbulb.png" alt="Lightbulb Graphic"></a>
+        <a href="/temp/temp.html"><img class="sloganBulb" src="./images/logos/lightbulb.png" alt="Lightbulb Graphic"></a>
          Ideas&nbsp;&nbsp;&nbsp;- -
       </span>
     </nav>
