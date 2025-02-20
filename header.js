5f338ff11e73f8d96f1788e8f5c1e152f578a4b1
halsan23 <halsan23@msn.com>
2025-02-20 16:09:13 -0700

    dd

--- header.js
+++ header.js
@@ -1,19 +1,18 @@
-let rootPath = '';
+// let rootPath = '';
 // var root = window.location.hostname;
 // console.log(`root = ${root}`);
 
-const getRoot = () => {
-  if (window.location.hostname === 'halsan23.github.io') {
-    return 'halsan23.github.io/bodingles';
-  } else {
-    return '.';
-  }
-}
+// const getRoot = () => {
+//   if (window.location.hostname === 'halsan23.github.io') {
+//     return 'halsan23.github.io/bodingles';
+//   } else {
+//     return '.';
+//   }
+// }
 
-rootPath = getRoot();
+// rootPath = getRoot();
 
-console.log(`rootPath = ${rootPath}`);
-console.log(`${rootPath}/images/scroll.png`);
+// console.log(`rootPath = ${rootPath}`);
 
 
 
@@ -22,10 +21,10 @@ let appHeader = `
       <span class="headLogo"></span>
       <span class="headTitle">Bo</span>
       <span class="headTitleb">dingles<sub>TM</sub></span>
-      <span class="headScroll"><img src="${rootPath}/images/scroll.png" alt=""></span>
+      <span class="headScroll"><img src="/images/scroll.png" alt=""></span>
       <span class="headSlogan">
         - -&nbsp;&nbsp;&nbsp;Bright
-        <a href="/temp/temp.html"><img class="sloganBulb" src="${rootPath}/images/logos/lightbulb.png" alt="Lightbulb Graphic"></a>
+        <a href="/temp/temp.html"><img class="sloganBulb" src="/images/logos/lightbulb.png" alt="Lightbulb Graphic"></a>
          Ideas&nbsp;&nbsp;&nbsp;- -
       </span>
     </nav>
