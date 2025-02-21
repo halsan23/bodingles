ddaa4a3c6adea8312e3b0886e85e514a438679a0
halsan23 <halsan23@msn.com>
2025-02-20 18:16:37 -0700

    updating to external header/footer files badDoggy

--- header.js
+++ header.js
@@ -4,7 +4,7 @@ const getRoot = () => {
   if (window.location.hostname === 'halsan23.github.io') {
     return 'halsan23.github.io/bodingles/';
   } else {
-    return './';
+    return '/';
   }
 }
 
@@ -15,10 +15,10 @@ let appHeader = `
       <span class="headLogo"></span>
       <span class="headTitle">Bo</span>
       <span class="headTitleb">dingles<sub>TM</sub></span>
-      <span class="headScroll"><img src="${rootPath}/images/scroll.png" alt=""></span>
+      <span class="headScroll"><img src="${rootPath}images/scroll.png" alt=""></span>
       <span class="headSlogan">
         - -&nbsp;&nbsp;&nbsp;Bright
-        <a href="/temp/temp.html"><img class="sloganBulb" src="${rootPath}/images/logos/lightbulb.png" alt="Lightbulb Graphic"></a>
+        <a href="/temp/temp.html"><img class="sloganBulb" src="${rootPath}images/logos/lightbulb.png" alt="Lightbulb Graphic"></a>
          Ideas&nbsp;&nbsp;&nbsp;- -
       </span>
     </nav>
