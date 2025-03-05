938d3625d94eb01ef1003926d6e7d91568ecea64
badDoggy <halsan23@msn.com>
2025-03-04 18:24:18 -0700

    ongoing work on color convertor badDoggy

--- courses/cSteele-FSWD-Bootcamp-2024/courseProjects/colorConvert/colorConvert.js
+++ courses/cSteele-FSWD-Bootcamp-2024/courseProjects/colorConvert/colorConvert.js
@@ -7,105 +7,88 @@
 // assign variables
 const form1 = document.querySelector('#form1');
 const form2 = document.querySelector('#form2');
-const label1 = document.querySelector('#label1');
-const label2 = document.querySelector('#label2');
-const butt1 = document.querySelector('#butt1');
-const butt2 = document.querySelector('#butt2');
+
 const colTyp = document.querySelector('#colorType');
 const colVal = document.querySelector('#colorValue');
-const placeholder = document.getElementById('form2-placeholder');
 
-form2.hidden = true;
+const butt1 = document.querySelector('#butt1');
+const butt2 = document.querySelector('#butt2');
+
+const colorsDisp = document.querySelector('#colorsDisp');
+const oppositeDisp = document.querySelector('#oppositeDisp');
 
 
-form1.addEventListener('submit', function (evt) {
-    evt.preventDefault();
 
-    form2.hidden = false;
-    butt1.hidden = true;
+const col1 = document.querySelector('#col1');
+const col2 = document.querySelector('#col2');
 
-    if (colTyp.value === 'rgb') {
-      rgbInput();
-    } else if (colTyp.value === 'hex') {
-      hexInput();
-    } else {
-      hslInput();
-    }
-});
+const val1 = document.querySelector('#val1');
+const val2 = document.querySelector('#val2');
 
-function rgbInput() {
-   label1.innerText = 'Enter RGB color value: rgb( ';
+// set click handler for color type submit
+const click1Handler = function(evt) {
+   evt.preventDefault();
+
+   //  remove button 1, show form2 and button 2
+   butt1.hidden = true;
+   form2.hidden = false;
+   butt2.hidden = false;
+
+   // remove button1 event listener (now hidden)
+   form1.removeEventListener('click', click1Handler, true);
+
+   // trap for color type choice
+   if (colTyp.value = 'rgb') {
+      rgbConvert();
+   }
+};
+
+// function to convert rgb to hex and hsl
+function rgbConvert() {
+   // set click handler for color value submit
+   const click2Handler = function(evt) {
+      evt.preventDefault();
+
+      // break input string down into individual numbers
+      let numbers = colVal.value.split(",")
+      let r = numbers[0]; g = numbers[1]; b = numbers[2];
+      console.log(`r =${r}, g =${g}, b = ${b}`);
+
+      // set color strings
+      let rgb = `rgb(${r}, ${g}, ${b})`;
+      console.log(`rgb: ${rgb}`);
+      // let hex = '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+      // let hex = rgbToHex(r, g, b);
+      let hex = rgbToHex(r, g, b);
+      console.log(`hex: ${hex}`);
+
+
+      // output results
+      colorsDisp.hidden = false;
+      col1.style.backgroundColor = rgb;
+      val1.innerText = rgb;
+      col2.style.backgroundColor = hex;
+      val2.innerText = hex;
+   };
+
+
    colVal.placeholder = '255,255,255';
-   colVal.style.width += 10;
-   label2.innerText = ' )';
+
+   // event listener for color value submit
+   form2.addEventListener('click', click2Handler, true);
 }
 
-function hexInput() {
-   label1.innerText = 'Enter HEX color value: #';
-   colVal.placeholder = 'ffffff';
-   label2.innerText = '';
-}
+const rgbToHex = (r, g, b) => {
+   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+ }
 
-function hslInput() {
-   label1.innerText = 'Enter HSL color value: ( ';
-   colVal.placeholder = '180, 100%, 100%';
-   label2.innerText = ' )';
-}
+// set initial defaults
+butt1.hidden = false;
+form2.hidden = true;
+butt2.hidden = true;
+colVal.innerText = '';
+colorsDisp.hidden = true;
+oppositeDisp.hidden = true;
 
-
-
-
-
-
-// colVal.style.width = placeholder.offsetWidth + "0px";
-
-
-
-
-
-
-
-// // Create the Class
-// class MakeColor {
-//    constructor ( r = 215, g = 170, b = 80, colorName = 'Bronze' ) {
-//          this.r = r;
-//          this.g = g;
-//          this.b = b;
-//          this.colorName = colorName;
-//          console.log(`color = ${name}: rgb(${r}, ${g}, ${b})`);
-//    }
-
-//    // create a name method
-//    name() {
-//       const colorName = this.colorName;
-//       return `Color Name is: ${colorName}`;
-//    }
-
-//    // create a "base" method to use in the rgb methods
-// 	innerRGB() {
-// 		const { r, g, b } = this;
-// 		return `${r}, ${g}, ${b}`;
-// 	}
-
-//    // create the rgb method
-// 	rgb() {
-// 		return `rgb(${this.innerRGB()})`;
-// 	}
-
-//    // create the rgba method
-// 	rgba(a = 1.0) {
-// 		return `rgba(${this.innerRGB()}, ${a})`;
-// 	}
-
-//    // create the hex method
-// 	hex() {
-//       const { r, g, b } = this;
-// 		return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-// 	}
-// }
-
-// // Call the Class
-// const color1 = new MakeColor();
-// const color2 = new MakeColor(0, 195, 195, 'Teal');
-
-// console.log('Available color variables are color1 (Bronze) and color2 (Teal).');
+// event listener for color type submit
+form1.addEventListener('click', click1Handler, true);
