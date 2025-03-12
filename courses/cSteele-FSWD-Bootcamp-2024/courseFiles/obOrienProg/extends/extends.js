cfd76bd3769030066334ddd93e25ffe1613eb1fb
halsan23 <halsan23@msn.com>
2025-03-12 09:56:46 -0600

    minor updates to extends & super cSteele

--- courses/cSteele-FSWD-Bootcamp-2024/courseFiles/obOrienProg/extends/extends.js
+++ courses/cSteele-FSWD-Bootcamp-2024/courseFiles/obOrienProg/extends/extends.js
@@ -1,46 +1,66 @@
 // Extends & Super keywords Demo
 // ======================================
 
-// create the Parent Constructor Class
+// create Parent constructor class
 class Pets {
-   constructor(name) {
+   constructor ( name, age ) {
       this.name = name;
+      this.age = age;
    }
+   // create the age method
+   age() {
+      return `${this.name} is ${this.age} years old.`;
+   }
+   // create the eat method
    eat() {
       return `${this.name} is eating.`;
    }
 }
 
-// dog Class
+// create Dog class
 class Dog extends Pets {
-   // bark method
+   // create the bark method
    bark() {
-      return `${this.name} says Woof, Woof, BARK, BARK`;
+      return `${this.name} goes Woof, Woof, Bark, Bark.`;
+   }
+   // create the modified eat method
+   eat() {
+      return `${this.name} scarf's his food.`;
    }
 }
 
-
-// cat Class
+// create Cat class
 class Cat extends Pets {
-	constructor( name, livesLeft = 9 ) {
-		super(name);
-		this.livesLeft = livesLeft;
-	}
-   // meow method
+   constructor ( name, age = 2, livesLeft = 9 ) {
+      super(name, age);
+      this.livesLeft = livesLeft;
+   }
+   // create the meow method
    meow() {
-      return `${this.name} says Meooww, purr, purr`;
+      return `${this.name} goes Meeooww, purr, purr.`;
+   }
+   // create the livesLeft method
+   lives() {
+      return `${this.name} has ${livesLeft} lives left.`;
    }
 }
 
 
 // create the class object's
-const myDog = new Dog('Chico');
+const myDog = new Dog('Chico', 10);
 const myCat = new Cat('Jitters');
+// log the dog/cat objects
+console.log(myDog);
+console.log(myCat);
+console.log(' ');
 
-// call the dog method
+// call the dog methods
+console.log(myDog.age());
 console.log(myDog.eat());
 console.log(myDog.bark());
+console.log(' ');
 
-// call the cat method
+// call the cat methods
+console.log(myCat.age);
 console.log(myCat.eat());
 console.log(myCat.meow());
