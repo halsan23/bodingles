06350ef6f9c27ee8b19ebdedc5f6fdc4290da2f1
badDoggy <halsan23@msn.com>
2025-03-17 15:05:30 -0600

    dd

--- courses/cSteele-FSWD-Bootcamp-2024/courseFiles/nodeNPM/jokes/index.js
+++ courses/cSteele-FSWD-Bootcamp-2024/courseFiles/nodeNPM/jokes/index.js
@@ -1,34 +1,14 @@
-// set variables to output joke to webpage
-const butt = document.querySelector('#butt');
-const newJoke = document.querySelector('#newJoke');
-
-const parButt = butt.parentElement;
-const parJoke = newJoke.parentElement;
-
 // import the joke package
 const jokes = require('give-me-a-joke');
-
 // import the color package
 const colors = require('colors');
 
 
 
-// get a joke
-parButt.addEventListener('click', function(evt) {
-   evt.preventDefault();
-
-   console.log('Button Clicked');
-
-   // To get a random dad joke
-   jokes.getRandomDadJoke (function(joke) {
-
-      //=> console.log(joke);
-      console.log(joke.green);
-
-      // output on webpage
-      parJoke.innerText = joke.green;
-   });
-
+// To get a random dad joke
+jokes.getRandomDadJoke (function(joke) {
+   //=> console.log(joke);
+   console.log(joke.green);
 });
 
 
