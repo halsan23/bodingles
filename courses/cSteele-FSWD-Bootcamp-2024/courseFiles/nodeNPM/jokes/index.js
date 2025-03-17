f44451698a5326adf137ed34bc023edb6fc87af2
halsan23 <halsan23@msn.com>
2025-03-17 12:27:43 -0600

    working on installing and using npm packages cSteele

---
+++ courses/cSteele-FSWD-Bootcamp-2024/courseFiles/nodeNPM/jokes/index.js
@@ -0,0 +1,34 @@
+// set variables to output joke to webpage
+const butt = document.querySelector('#butt');
+const newJoke = document.querySelector('#newJoke');
+
+const parButt = butt.parentElement;
+const parJoke = newJoke.parentElement;
+
+// import the joke package
+const jokes = require('give-me-a-joke');
+
+// import the color package
+const colors = require('colors');
+
+
+
+// get a joke
+parButt.addEventListener('click', function(evt) {
+   evt.preventDefault();
+
+   // To get a random dad joke
+   jokes.getRandomDadJoke (function(joke) {
+
+      //=> console.log(joke);
+      console.log(joke.green);
+
+      // output on webpage
+      parJoke.innerText = joke.green;
+   });
+
+});
+
+
+
+
