96a0ee27ce229af92f0c3fe5b889663eaecafb87
badDoggy <halsan23@msn.com>
2025-03-18 17:05:00 -0600

    dd

--- courses/cSteele-FSWD-Bootcamp-2024/courseFiles/node/packageJson/figlet/index.js
+++ courses/cSteele-FSWD-Bootcamp-2024/courseFiles/node/packageJson/figlet/index.js
@@ -5,10 +5,13 @@ const figlet = require('figlet');
 const artText = 'Hello World!!'
 
 figlet(artText, function ( err, data ) {
+   // check for error
    if (err) {
-   console.log("Something went wrong...");
-   console.dir(err);
-   return;
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
    }
+
+   // if no error, log the data
    console.log(data);
-   })
+})
