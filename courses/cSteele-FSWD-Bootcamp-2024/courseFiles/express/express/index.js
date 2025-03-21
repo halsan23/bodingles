d8eb9bfb85366032e2bb6a184b9446c289cd3fdb
badDoggy <halsan23@msn.com>
2025-03-21 14:26:55 -0600

    finished Express req & res started Express routing cSteele

--- courses/cSteele-FSWD-Bootcamp-2024/courseFiles/express/express/index.js
+++ courses/cSteele-FSWD-Bootcamp-2024/courseFiles/express/express/index.js
@@ -1,11 +1,23 @@
+// define the variables
+
+// require (import) express
 const express = require('express');
+
+// app variable represents return values from express object
 const app = express();
+
+// assign the localhost server port #
+// usually 8080 or 3000
 const port = 3000;
 
+
+// process any 'get' responses
+// app.get(/(.*)/, (req, res) => {   // bug fix
 app.get('/', (req, res) => {
-   res.send('Hello World!')
+   res.send('<h3>Hello World!</h3>')
 })
 
+// start the server and wait for a response
 app.listen(port, () => {
    console.log(`App Server is running : Listening on port ${port}`)
 })
