f35d11729752fe9c9e24462d1605a65ade4f317d
halsan23 <halsan23@msn.com>
2025-03-23 11:31:07 -0600

    working on Dynamic Templating with EJS cSteele

---
+++ courses/cSteele-FSWD-Bootcamp-2024/courseFiles/templating/express/index.js
@@ -0,0 +1,20 @@
+// Set the variables and requires
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+// set ejs as our view engine using .get() express method
+app.set( 'view engine', 'ejs' );
+
+
+// get request for root dir
+app.get( '/', ( req, res ) => {
+   res.send('Responding on Root Directory.')
+})
+
+
+// Start Server : Listening on {port}
+app.listen( port, () => {
+   console.log(`Server Online : Listening on port: ${port}`);
+});
