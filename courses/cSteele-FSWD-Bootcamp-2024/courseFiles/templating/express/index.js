9651ae7351036cccc18a568afea57947adebd56f
halsan23 <halsan23@msn.com>
2025-03-24 12:10:53 -0600

    working Templating, Express, and EJS cSteele

--- courses/cSteele-FSWD-Bootcamp-2024/courseFiles/templating/express/index.js
+++ courses/cSteele-FSWD-Bootcamp-2024/courseFiles/templating/express/index.js
@@ -2,10 +2,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require( 'path' );
 
 
 // set ejs as our view engine using .get() express method
 app.set( 'view engine', 'ejs' );
+// define our "path" to the views sub-directory
+app.set( 'views', path.join( __dirname, '/views' ));
 
 
 // get request for root dir
