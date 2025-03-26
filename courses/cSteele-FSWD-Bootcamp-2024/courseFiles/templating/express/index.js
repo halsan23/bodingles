a7958179a40e59eb81009d1c45817ed8832a8af7
halsan23 <halsan23@msn.com>
2025-03-26 12:13:18 -0600

    working on subReddit ejs template cSteele

--- courses/cSteele-FSWD-Bootcamp-2024/courseFiles/templating/express/index.js
+++ courses/cSteele-FSWD-Bootcamp-2024/courseFiles/templating/express/index.js
@@ -1,8 +1,9 @@
 // Set the variables and requires
 const express = require('express');
 const app = express();
-const port = 3000;
 const path = require( 'path' );
+const port = 3000;
+const demoData = require( './ejsDemoData.json' );
 
 
 // set ejs as our view engine using .get() express method
@@ -11,10 +12,28 @@ app.set( 'view engine', 'ejs' );
 app.set( 'views', path.join( __dirname, '/views' ));
 
 
-// get request for root dir
+// get request for root view
 app.get( '/', ( req, res ) => {
    const num = Math.floor(Math.random() * 100) + 1;
    res.render('home', { num })
+})
+
+// get request for pets view
+app.get( '/pets', ( req, res ) => {
+   const pets = [ 'Chico', 'Bella', 'Sadie', 'Addie Cakes', 'Boo Boo', 'Jasper', 'Jitters' ];
+   res.render('pets', { pets })
+})
+
+// get request for subreddit view
+app.get( '/r/:subreddit', ( req, res ) => {
+   const { subreddit } = req.params;
+   const data = demoData[subreddit];
+   if (data) {
+      // the 3 dots allow us to "spread" the data to get to the individual pieces, such as "Name"
+      res.render('subReddit', { ...data });
+   } else {
+      res.render('notFound', { subreddit });
+   }
 })
 
 
