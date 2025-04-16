dc42555f1010ddc022e25b563fb45b8ccc6ccb94
badDoggy <halsan23@msn.com>
2025-04-16 14:56:04 -0600

    ongoing mongoose insert one / insert many cSteele

--- courses/cSteele-2025/courseFiles/mongoose/express/index.js
+++ courses/cSteele-2025/courseFiles/mongoose/express/index.js
@@ -23,12 +23,25 @@ const movieSchema = new mongoose.Schema({
 // create a movie class based on the movie model
 // the model is: mongoose.Model('Movie', movieSchema)
 // assigning it to a variable creates the movie class
-const movie = mongoose.model('Movie', movieSchema);
+const Movie = mongoose.model('Movie', movieSchema);
 
 // Create a new instance of the movie class
-const badSanta = new movie({
+const badSanta = new Movie({
    title: 'Bad Santa',
    year: 2003,
    score: 7.0,
    rating: 'R'
 })
+
+// insert MANY movies at once
+// Movie.insertMany([
+//     { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+//     { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+//     { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+//     { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+//     { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
+// ])
+//     .then(data => {
+//         console.log("Movie Data Uploaded:");
+//         console.log(data);
+//     })
