64cb1573e2096b8ab64137201f6309822479aad3
badDoggy <halsan23@msn.com>
2025-05-16 16:20:12 -0600

    dd

---
+++ temp/temp.js
@@ -0,0 +1,18 @@
+function lifeInWeeks(age) {
+
+/************Don't change the code above************/
+
+    //Write your code here.
+   let years = 90 - age;
+   let days = years * 365;
+   let weeks = years * 52;
+   let months = years * 12;
+   console.log(`You have ${days} days, ${weeks} weeks, and ${months} months left.`)
+
+/*************Don't change the code below**********/
+}
+
+
+
+
+lifeInWeeks(68);
