d0a9630f2410e6c5f24769e8856877f5dc9b3423
badDoggy <halsan23@msn.com>
2025-05-18 12:22:24 -0600

    dd

--- temp/temp.js
+++ temp/temp.js
@@ -1,18 +1,18 @@
 function leapYear(year) {
    if ((year % 4) === 0) {
       if ((year % 100) === 0 && (year % 400) === 0) {
-         console.log(`The year ${year} is a Leap Year.`);
+         return `The year ${year} is a Leap Year.`;
       } else {
          if ((year % 100) !== 0 || (year % 400) === 0) {
-            console.log(`The year ${year} is a Leap Year.`);
+            return `The year ${year} is a Leap Year.`;
          } else {
-            console.log(`The year ${year} is not a Leap Year.`);
+            return `The year ${year} is not a Leap Year.`;
          }
       }
    } else {
-      console.log(`The year ${year} is not a Leap Year.`);
+         return `The year ${year} is not a Leap Year.`;
    }
 }
 
 
-leapYear(1963);
+console.log( leapYear(1963) );
