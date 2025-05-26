c5e164d14e743b685d5abc7d919d3f315087f1d1
halsan23 <halsan23@msn.com>
2025-05-26 11:37:27 -0600

    dd

--- courses/angYu/courseProjects/drumRoll/drumRoll.js
+++ courses/angYu/courseProjects/drumRoll/drumRoll.js
@@ -1,10 +1,33 @@
-let btn = ['w', 'a', 's', 'd', 'j', 'k', 'l']
+// let btn = ['w', 'a', 's', 'd', 'j', 'k', 'l']
 
-// loop through all drums to add event listener
-for ( let i = 0; i < document.querySelectorAll('.drum').length; i++) {
+// // loop through all drums to add event listener
+// for ( let i = 0; i < document.querySelectorAll('.drum').length; i++) {
 
-   // add event listeners
-   document.querySelectorAll('.drum')[i].addEventListener('click', function () {
-      alert(`${btn[i]} was clicked`);
-   });
+//    // add event listeners
+//    document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+//       alert(`${btn[i]} was clicked`);
+//    });
+// }
+
+
+
+
+function add ( num1, num2 ) {
+   return num1 + num2;
+}
+
+function subt ( num1, num2 ) {
+   return num1 - num2;
+}
+
+function mult ( num1, num2 ) {
+   return num1 * num2;
+}
+
+function divide ( num1, num2 ) {
+   return num1 / num2;
+}
+
+function calc ( num1, num2, operator ) {
+   return operator( num1, num2 );
 }
