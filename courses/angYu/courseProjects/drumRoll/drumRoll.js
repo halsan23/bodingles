8cba6e158845b36c75c9adab1377af13ab5dc301
halsan23 <halsan23@msn.com>
2025-05-30 11:21:46 -0600

    dd

--- courses/angYu/courseProjects/drumRoll/drumRoll.js
+++ courses/angYu/courseProjects/drumRoll/drumRoll.js
@@ -1,13 +1,37 @@
-// loop through all drums to add event listener
+// Drum Roll
+// ========================================================================================
+
+
+// add "click trap" to process id drum was "clicked"
+// ----------------------------------------------------------------------------------------
+// loop through all drums to add "click" event listener
 for ( let i = 0; i < document.querySelectorAll('.drum').length; i++) {
 
-   // add event listener for each individual button
+   // add event listener for each individual html button
    document.querySelectorAll('.drum')[i].addEventListener('click', function () {
 
       // "this" represents the individual button that was clicked
       // this.innerHTML represents the letter that was set inside the html code for each individual button
-      // so we are "trapping" for the individual letter of each button
-      switch (this.innerHTML) {
+      // so we are "trapping" for the individual letter of each html button
+      let buttonInnerHTML = this.innerHTML;
+
+      // call th function to actually produce the sound
+      // based on html button "click"
+      makeSound(buttonInnerHTML);
+
+   });
+}
+
+document.addEventListener('keydown', (evt) => {
+   // call th function to actually produce the sound
+   // based on "key press"
+   makeSound(evt.key);
+});
+
+// play the sound based on html button clicked,
+// or key pressed
+function makeSound (key) {
+   switch (key) {
          case "w":
             let tom1 = new Audio(`./sounds/tom-1.mp3`);
             tom1.play();
@@ -38,8 +62,6 @@ for ( let i = 0; i < document.querySelectorAll('.drum').length; i++) {
             break;
 
          default:
-            alert(`Incorrect Key [${this.innerHTML}] Pressed!!`);
+            alert(`Incorrect Key "${key}" Pressed!!`);
       }
-
-   });
 }
