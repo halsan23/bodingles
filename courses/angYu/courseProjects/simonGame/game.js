a9e598c51e842006cb78c118664b98383e2350ed
badDoggy <halsan23@msn.com>
2025-06-10 16:03:51 -0600

    ongoing Simon Game aYu / badDoggy

--- courses/angYu/courseProjects/simonGame/game.js
+++ courses/angYu/courseProjects/simonGame/game.js
@@ -3,33 +3,53 @@
 // ==========================================================
 
 
-// Game Setup
+// Set Initial Variables
 // ----------------------------------------------------------
 let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
+let level = 0;
 
-$("btn").click( function() {
-   let userChosenColour = $("this").attr("id");
+
+$(document).keypress( function() {
+   if (level === 0) {
+      nextSequence();
+   }
+});
+
+
+$(".btn").click( function() {
+
+   let userChosenColour = $(this).attr("id");
    userClickedPattern.push(userChosenColour);
 
    playSound(userChosenColour);
+   animatePress(userChosenColour);
 });
 
 
 function nextSequence() {
+   level++;
+   $("#level-title").text(`Level ${level}`);
+
    let randomNumber = Math.floor(Math.random() * 4 );
    let randomChosenColour = buttonColours[randomNumber];
    gamePattern.push(randomChosenColour);
 
-   // Button Flash on button that matches -randomChosenColour-
    $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
-
    playSound(randomChosenColour);
 }
+
 
 function playSound(name) {
    let audio = new Audio(`sounds/${name}.mp3`);
    audio.play();
 }
-// ----------------------------------------------------------
+
+
+function animatePress(currentColour) {
+   $(`#${currentColour}`).addClass("pressed");
+   setTimeout(function() {
+      $(`#${currentColour}`).removeClass("pressed");
+   }, 100);
+}
