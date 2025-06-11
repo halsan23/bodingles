c56f31c135ff04daea0e5180dfe9c97ace9decbf
badDoggy <halsan23@msn.com>
2025-06-11 16:10:47 -0600

    ongoing Simon Game aYu | badDoggy

--- courses/angYu/courseProjects/simonGame/game.js
+++ courses/angYu/courseProjects/simonGame/game.js
@@ -11,6 +11,7 @@ let userClickedPattern = [];
 let level = 0;
 
 
+// press any key to start the game
 $(document).keypress( function() {
    if (level === 0) {
       nextSequence();
@@ -18,41 +19,52 @@ $(document).keypress( function() {
 });
 
 
+// click function for color buttons
 $(".btn").click( function() {
+   // which button was clicked - assign to variable
    let userChosenColor = $(this).attr("id");
    userClickedPattern.push(userChosenColor);
-
    // console.log(`User Pattern: ${userClickedPattern}`);
 
+   // play sound and animate button that was clicked
    playSound(userChosenColor);
    animatePress(userChosenColor);
 
    // console.log(`Last Index: ${(userClickedPattern.length) - 1}`)
+
+   // check is user sequence is correct
    checkAnswer(userClickedPattern.length - 1);
 });
 
 
+// continue playing game
 function nextSequence() {
+   // increase level # and output it as text
    level++;
    $("#level-title").text(`Level ${level}`);
 
+   // reset user input sequence
    userClickedPattern = [];
 
+   // generate random game color and add it to the game sequence array
    let randomNumber = Math.floor(Math.random() * 4 );
    let randomChosenColor = buttonColors[randomNumber];
    gamePattern.push(randomChosenColor);
 
+   // flash button for game generated button and play sound
    $(`#${randomChosenColor}`).fadeIn(100).fadeOut(100).fadeIn(100);
    playSound(randomChosenColor);
 }
 
 
+// play the audion that matches the button clicked
 function playSound(name) {
    let audio = new Audio(`sounds/${name}.mp3`);
    audio.play();
 }
 
 
+// add animated flash effect for button that was clicked
 function animatePress(currentColor) {
    $(`#${currentColor}`).addClass("pressed");
    setTimeout(function() {
@@ -60,6 +72,8 @@ function animatePress(currentColor) {
    }, 100);
 }
 
+
+// check for win / loss scenario
 function checkAnswer(currentLevel) {
    console.log(`Game Pattern = ${gamePattern}`)
    console.log(`User Pattern = ${userClickedPattern}`)
