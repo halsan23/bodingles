034581c0380dd24a1b4f7ed2a3150b58222f3bb1
badDoggy <halsan23@msn.com>
2025-06-28 14:33:52 -0600

    ongoing Express GET requests nodemon badDoggy | aYu

--- courses/angYu/courseFiles/express/expressServer/index.js
+++ courses/angYu/courseFiles/express/expressServer/index.js
@@ -4,11 +4,20 @@ const app = express();
 const port = 3000;
 
 
-// get request
+// get request /(root)
 app.get("/", (req, res) => {
-   res.send("Hello World");
+   res.send("<h1>Home Page</h1>");
 });
 
+// get request /about
+app.get("/about", (req, res) => {
+   res.send("<h1>About Page</h1>");
+});
+
+// get request /contact
+app.get("/contact", (req, res) => {
+   res.send("<h1>Contact Page</h1>");
+});
 
 // setup listening port
 // when we run this file, it starts the server
