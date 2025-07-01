4c93a6f8eac035241b8239abb274f278b8cd302c
badDoggy <halsan23@msn.com>
2025-07-01 15:19:19 -0600

    ongoing express middleware badDoggy | aYu

--- courses/angYu/courseFiles/express/middleware/index.js
+++ courses/angYu/courseFiles/express/middleware/index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
