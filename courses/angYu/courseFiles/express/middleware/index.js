35710da788d8eed2a4459746cc20ad3850980016
halsan23 <halsan23@msn.com>
2025-07-03 11:55:00 -0600

    ongoing express middleware badDoggy

--- courses/angYu/courseFiles/express/middleware/index.js
+++ courses/angYu/courseFiles/express/middleware/index.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 
 // get request for home page
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "index.html");
 });
 
 
