f7f83f4982ccdd79f2558c2357bfe036bdacde00
badDoggy <halsan23@msn.com>
2025-08-01 16:44:18 -0600

    ongoing weather API badDoggy

--- courses/angYu/courseProjects/weatherAPI/index.js
+++ courses/angYu/courseProjects/weatherAPI/index.js
@@ -2,45 +2,42 @@ import express from 'express';
 import ejs from 'ejs';
 import axios from 'axios';
 
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Open Weather API Key
+const API_KEY = "a443ef5ce9ff06d140f287deeb37e819";
+
 app.use(express.static('public'));
 
-
-async function getLocation(city) {
-   const geoLoc = await fetch(geoLoc_URL);
-   const result = geoLoc.data;
+async function geoLoc(city) {
+   const res = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`);
    return {
-         name: result.name || "",
-         lat: result.latitude,
-         lon: result.longitude
+      name: res.data[0].name || "",
+      lat: res.data[0].lat,
+      lon: res.data[0].lon
    }
 }
-
-const city = "Rock Springs";
-
-const geoLoc_URL = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en`;
-
-const {name, lat, lon} = getLocation(city);
-
-const weather_URL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,is_day,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min`;
-
 
 
 app.get("/", async ( req, res ) => {
    try {
-      console.log(`name: ${name}`);
-      console.log(`lat: ${lat}`);
-      console.log(`lon: ${lon}`);
-
-      // const result = await axios.get(weather_URL);
-      // res.render('index.ejs', { content: result.data });
-   } catch (err) {
-      console.log(err.message);
-      res.render('index.ejs', { content: err.message });
+      const {name, lat, lon} = await geoLoc("rock springs");
+      const data = {
+         name: name,
+         lat: lat,
+         lon: lon
+      };
+      res.render('index.ejs', { content: JSON.stringify(data) });
+   } catch (error) {
+      res.render('index.ejs', { content: error.message });
    }
-});
+})
+
+
+
+
 
 
 app.listen( port, () => console.log(`Server listening on port ${port}`));
