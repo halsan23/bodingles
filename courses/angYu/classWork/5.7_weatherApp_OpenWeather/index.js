c8f0c5597c68e1cda519e01a8d8591a8616e16d0
halsan23 <halsan23@msn.com>
2025-08-02 12:00:52 -0600

    ongoing Dinkle Weatherby weather api badDoggy

--- courses/angYu/classWork/5.7_weatherApp_OpenWeather/index.js
+++ courses/angYu/classWork/5.7_weatherApp_OpenWeather/index.js
@@ -6,20 +6,23 @@ import axios from 'axios';
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Open Weather API Key
-const API_KEY = "a443ef5ce9ff06d140f287deeb37e819";
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// Open Weather API Key
+// const API_KEY = "a443ef5ce9ff06d140f287deeb37e819";
+
+
 async function geoLoc(city) {
-   const res = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`);
-   console.log(`City: ${res.data[0].name}`);
-   console.log(`Latitude: ${res.data[0].lat}`);
-   console.log(`Longitude: ${res.data[0].lon}`);
-   return {
-      name: res.data[0].name || "",
-      lat: res.data[0].lat,
-      lon: res.data[0].lon
+   try {
+      const result = await axios.get(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en&format=json`);
+      return {
+         name: result.data.results[0].name || "",
+         lat: result.data.results[0].latitude,
+         lon: result.data.results[0].longitude
+      }
+   } catch (err) {
+
    }
 }
 
@@ -28,22 +31,32 @@ app.get("/", async ( req, res ) => {
    try {
       const {name, lat, lon} = await geoLoc("rock springs");
 
-      const result = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily,alerts&appid=${API_KEY}`)
+      const result = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_min,temperature_2m_max&models=gfs_seamless&current=temperature_2m,relative_humidity_2m,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&timezone=auto&forecast_days=1&wind_speed_unit=mph&precipitation_unit=inch&temperature_unit=fahrenheit`);
 
-      console.log(result.data[0]);
-      // const data = {
-      //    name: name,
-      //    lat: lat,
-      //    lon: lon
-      // };
-      // res.render('index.ejs', { content: JSON.stringify(data) });
-      res.render('index.ejs');
+      // console.log(result.data);
+
+      let weatherData = {
+         name: name,
+         weather: result.data
+      }
+
+      res.render('index.ejs', { content: JSON.stringify(weatherData) });
+
+
    } catch (error) {
       res.render('index.ejs', { content: error.message });
    }
 })
 
 
+app.post("/", (req, res) => {
+   try {
+      const city = req.body["location"];
+      console.log(city);
+   } catch (err) {
+      console.log(err);
+   }
+});
 
 
 
