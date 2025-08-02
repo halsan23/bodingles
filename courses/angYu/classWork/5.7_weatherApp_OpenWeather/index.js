0fc217f89a6f82437d2da4daede2943b8a5d5c4a
badDoggy <halsan23@msn.com>
2025-08-02 15:37:43 -0600

    ongoing Dinkle Weatherby's Weather Pronosticator badDoggy

--- courses/angYu/classWork/5.7_weatherApp_OpenWeather/index.js
+++ courses/angYu/classWork/5.7_weatherApp_OpenWeather/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
-import ejs from 'ejs';
 import axios from 'axios';
-
+import ejs from 'ejs';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,56 +8,54 @@ const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// Open Weather API Key
-// const API_KEY = "a443ef5ce9ff06d140f287deeb37e819";
-
 
 async function geoLoc(city) {
-   try {
-      const result = await axios.get(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en&format=json`);
-      return {
-         name: result.data.results[0].name || "",
-         lat: result.data.results[0].latitude,
-         lon: result.data.results[0].longitude
-      }
-   } catch (err) {
-
+   const result = await axios.get(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en&format=json`);
+   return {
+      name: result.data.results[0].name || "",
+      lat: result.data.results[0].latitude,
+      lon: result.data.results[0].longitude
    }
 }
 
 
-app.get("/", async ( req, res ) => {
-   try {
-      const {name, lat, lon} = await geoLoc("rock springs");
+app.get('/', async (req, res) => {
+   res.render('index.ejs');
+})
 
-      const result = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_min,temperature_2m_max&models=gfs_seamless&current=temperature_2m,relative_humidity_2m,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&timezone=auto&forecast_days=1&wind_speed_unit=mph&precipitation_unit=inch&temperature_unit=fahrenheit`);
+
+app.post('/', async (req, res) => {
+   let city = req.body.location;
+   console.log(city);
+
+
+   try {
+      const {name, lat, lon} = await geoLoc(city);
+      console.log(name);
+      console.log(lat);
+      console.log(lon);
+
+      // const result = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_min,temperature_2m_max&models=gfs_seamless&current=temperature_2m,relative_humidity_2m,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&timezone=auto&forecast_days=1&wind_speed_unit=mph&precipitation_unit=inch&temperature_unit=fahrenheit`);
 
       // console.log(result.data);
 
       let weatherData = {
          name: name,
-         weather: result.data
+         lat: lat,
+         lon: lon
+         // weather: result.data
       }
 
       res.render('index.ejs', { content: JSON.stringify(weatherData) });
 
 
-   } catch (error) {
-      res.render('index.ejs', { content: error.message });
+   } catch (err) {
+      let weatherData = {
+         error: "Location Not Found"
+      }
+      res.render('index.ejs', { content: JSON.stringify(weatherData) });
    }
 })
-
-
-app.post("/", (req, res) => {
-   try {
-      const city = req.body["location"];
-      console.log(city);
-   } catch (err) {
-      console.log(err);
-   }
-});
-
-
 
 
 app.listen( port, () => console.log(`Server listening on port ${port}`));
