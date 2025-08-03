6a97e66f3ff16b14e991f739047dedc91877d5f4
halsan23 <halsan23@msn.com>
2025-08-03 11:12:08 -0600

    weather app - minor directory change badDoggy

--- courses/angYu/classWork/5.7_weatherApp_OpenMeteo/index.js
+++ courses/angYu/classWork/5.7_weatherApp_OpenMeteo/index.js
@@ -8,9 +8,12 @@ const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// ================================================================================= //
 
-async function geoLoc(city) {
-   const result = await axios.get(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en&format=json`);
+// get latitude, longitude from city name
+async function geoLoc(location) {
+   const result = await axios.get(`https://geocoding-api.open-meteo.com/v1/search?name=${location}&count=1&language=en&format=json`);
+
    return {
       name: result.data.results[0].name || "",
       lat: result.data.results[0].latitude,
@@ -25,28 +28,22 @@ app.get('/', async (req, res) => {
 
 
 app.post('/', async (req, res) => {
-   let city = req.body.location;
-   console.log(city);
-
-
+   let location = req.body.location;
    try {
-      const {name, lat, lon} = await geoLoc(city);
-      console.log(name);
-      console.log(lat);
-      console.log(lon);
+      const {name, lat, lon} = await geoLoc(location);
 
       // const result = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_min,temperature_2m_max&models=gfs_seamless&current=temperature_2m,relative_humidity_2m,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&timezone=auto&forecast_days=1&wind_speed_unit=mph&precipitation_unit=inch&temperature_unit=fahrenheit`);
 
       // console.log(result.data);
 
-      let weatherData = {
-         name: name,
-         lat: lat,
-         lon: lon
-         // weather: result.data
-      }
+      // let weatherData = {
+      //    name: name,
+      //    lat: lat,
+      //    lon: lon
+      //    // weather: result.data
+      // }
 
-      res.render('index.ejs', { content: JSON.stringify(weatherData) });
+      // res.render('index.ejs', { content: JSON.stringify(weatherData) });
 
 
    } catch (err) {
