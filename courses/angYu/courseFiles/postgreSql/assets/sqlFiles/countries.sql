be680347a8333086b9e57419a5d7197776c7e5ba
badDoggy <halsan23@msn.com>
2025-08-18 16:11:37 -0600

    ongoing PostgreSQL badDoggy | aYu

--- courses/angYu/courseFiles/postgreSql/assets/sqlFiles/countries.sql
+++ courses/angYu/courseFiles/postgreSql/assets/sqlFiles/countries.sql
@@ -4,15 +4,6 @@ CREATE TABLE capitals (
 	capital VARCHAR(45)
 );
 
-CREATE TABLE flags (
-	id SERIAL PRIMARY KEY,
-	name text,
-	flag text
-);
-
 
 SELECT * FROM public.capitals
-ORDER BY id ASC
-
-SELECT * FROM public.flags
 ORDER BY id ASC
