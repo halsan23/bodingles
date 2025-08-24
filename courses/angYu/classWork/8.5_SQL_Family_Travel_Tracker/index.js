6b4f16de5342277f9ce53686d97e2e101394e940
halsan23 <halsan23@msn.com>
2025-08-24 12:01:53 -0600

    setup for "Family" Travel Tracker app badDoggy | aYu

--- courses/angYu/classWork/8.5_SQL_Family_Travel_Tracker/index.js
+++ courses/angYu/classWork/8.5_SQL_Family_Travel_Tracker/index.js
@@ -1,39 +1,41 @@
-import express from "express";
-import bodyParser from "body-parser";
-import pg from "pg";
-
+import express from 'express';
+import pg from 'pg';
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "world",
-  password: "123456",
-  port: 5432,
+   user: "postgres",
+   password: "password",
+   host: "localhost",
+   database: "world",
+   port: 5432
 });
 db.connect();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let currentUserId = 1;
 
 let users = [
-  { id: 1, name: "Angela", color: "teal" },
-  { id: 2, name: "Jack", color: "powderblue" },
+  { id: 1, name: "Angela", color: "#71c2d0ff" },
+  { id: 2, name: "Jack", color: "#a5ecf9ff" },
 ];
 
-async function checkVisisted() {
+// ============================================================================ //
+
+async function checkVisited() {
   const result = await db.query("SELECT country_code FROM visited_countries");
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
   return countries;
-}
+};
+
+
 app.get("/", async (req, res) => {
-  const countries = await checkVisisted();
+  const countries = await checkVisited();
   res.render("index.ejs", {
     countries: countries,
     total: countries.length,
@@ -41,6 +43,8 @@ app.get("/", async (req, res) => {
     color: "teal",
   });
 });
+
+
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
 
@@ -65,13 +69,15 @@ app.post("/add", async (req, res) => {
     console.log(err);
   }
 });
+
+
 app.post("/user", async (req, res) => {});
+
 
 app.post("/new", async (req, res) => {
   //Hint: The RETURNING keyword can return the data that was inserted.
   //https://www.postgresql.org/docs/current/dml-returning.html
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+
+app.listen(port, () => console.log(`Server Listening on localhost:${port}`));
