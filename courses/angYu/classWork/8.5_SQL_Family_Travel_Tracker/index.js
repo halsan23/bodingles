3dba686a063abc6b93bac1f7280521397157d1ad
badDoggy <halsan23@msn.com>
2025-08-25 16:24:07 -0600

    working on "Family" Travel Tracker badDoggy | aYu

--- courses/angYu/classWork/8.5_SQL_Family_Travel_Tracker/index.js
+++ courses/angYu/classWork/8.5_SQL_Family_Travel_Tracker/index.js
@@ -1,14 +1,15 @@
-import express from 'express';
-import pg from 'pg';
+import express from "express";
+import pg from "pg";
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 const db = new pg.Client({
-   user: "postgres",
-   password: "password",
-   host: "localhost",
-   database: "world",
-   port: 5432
+  user: "postgres",
+  password: "password",
+  host: "localhost",
+  database: "world",
+  port: 5432
 });
 db.connect();
 
@@ -16,37 +17,38 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let currentUserId = 1;
-
-let users = [
-  { id: 1, name: "Angela", color: "#71c2d0ff" },
-  { id: 2, name: "Jack", color: "#a5ecf9ff" },
-];
-
-// ============================================================================ //
-
+let users = '';
 async function checkVisited() {
-  const result = await db.query("SELECT country_code FROM visited_countries");
+  const result = await db.query(
+    "SELECT country_code FROM visited_countries JOIN users ON users.id = user_id WHERE user_id = $1; ",
+    [currentUserId]
+  );
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
   return countries;
-};
+}
 
+async function getCurrentUser() {
+  const result = await db.query("SELECT * FROM users");
+  users = result.rows;
+  return users.find((user) => user.id == currentUserId);
+}
 
 app.get("/", async (req, res) => {
   const countries = await checkVisited();
+  const currentUser = await getCurrentUser();
   res.render("index.ejs", {
     countries: countries,
     total: countries.length,
     users: users,
-    color: "teal",
+    color: currentUser.color,
   });
 });
-
-
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
+  const currentUser = await getCurrentUser();
 
   try {
     const result = await db.query(
@@ -58,8 +60,8 @@ app.post("/add", async (req, res) => {
     const countryCode = data.country_code;
     try {
       await db.query(
-        "INSERT INTO visited_countries (country_code) VALUES ($1)",
-        [countryCode]
+        "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
+        [countryCode, currentUserId]
       );
       res.redirect("/");
     } catch (err) {
@@ -70,14 +72,28 @@ app.post("/add", async (req, res) => {
   }
 });
 
-
-app.post("/user", async (req, res) => {});
-
-
-app.post("/new", async (req, res) => {
-  //Hint: The RETURNING keyword can return the data that was inserted.
-  //https://www.postgresql.org/docs/current/dml-returning.html
+app.post("/user", async (req, res) => {
+  if (req.body.add === "new") {
+    res.render("new.ejs");
+  } else {
+    currentUserId = req.body.user;
+    res.redirect("/");
+  }
 });
 
+app.post("/new", async (req, res) => {
+  const name = req.body.name;
+  const color = req.body.color;
+
+  const result = await db.query(
+    "INSERT INTO users (name, color) VALUES($1, $2) RETURNING *;",
+    [name, color]
+  );
+
+  const id = result.rows[0].id;
+  currentUserId = id;
+
+  res.redirect("/");
+});
 
 app.listen(port, () => console.log(`Server Listening on localhost:${port}`));
