aa564c64928a0ae6280b87d7de3f41d2a1a26a6e
badDoggy <halsan23@msn.com>
2025-08-30 17:13:52 -0600

    dd

--- courses/angYu/classWork/capstone2_book_project/index.js
+++ courses/angYu/classWork/capstone2_book_project/index.js
@@ -36,12 +36,19 @@ app.post('/', async (req, res) => {
       console.log(`Search String: ${searchString}`);
 
    try {
-      const result = await axios.get(`https://openlibrary.org/search.json?q=${searchString}`);
-      const bookData = result.data;
-      console.log(`BookData: ${bookData}`);
+      const response = await axios.get(`https://openlibrary.org/search.json?q=${searchString}&limit=1`);
+      const result = response.data;
+      const olid = result.docs[0].cover_edition_key;
+      const title = result.docs[0].title;
+      const published = result.docs[0].first_publish_year;
+      console.log(`OLID: ${olid}`);
+      console.log(`Title: ${title}`);
+      console.log(`First Published: ${published}`);
 
-      // const bookJson = JSON.stringify(bookData);
-      // console.log(`Book.JSON: ${bookData}`);
+      const coverImg = `https://covers.openlibrary.org/b/olid/${olid}-M.jpg`;
+      console.log(`Cover Image: ${coverImg}`);
+
+
 
    } catch (err) {
       console.log(err);
