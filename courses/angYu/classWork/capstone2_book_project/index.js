a79592b26222109a7eb578d762ce8a845e3d7a49
badDoggy <halsan23@msn.com>
2025-08-31 15:36:40 -0600

    dd

--- courses/angYu/classWork/capstone2_book_project/index.js
+++ courses/angYu/classWork/capstone2_book_project/index.js
@@ -20,20 +20,20 @@ db.connect();
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-// ========================================================================================//
-
+// ===================================================================//
 async function getBook(book) {
    try {
       const response = await axios.get(`https://openlibrary.org/search.json?q=${book}&limit=1`);
       const result = response.data;
+      const olid = result.docs[0].cover_edition_key;
+      const bookCover = `https://covers.openlibrary.org/b/olid/${olid}-M.jpg`;
 
       let bookData = {
-         olid: result.docs[0].cover_edition_key,
+         olid: olid,
          title: result.docs[0].title,
          published: result.docs[0].first_publish_year,
-         // coverImg: `https://covers.openlibrary.org/b/olid/${olid}-M.jpg`,
+         cover: bookCover
       }
-   console.log(`Book Data: ${bookData}`);
       return bookData;
    } catch (err) {
       console.log('Book not Found');
@@ -41,18 +41,25 @@ async function getBook(book) {
 }
 
 async function getDescr(olid) {
+   console.log(`Book olid: ${olid}`);
+
    try {
       const response = await axios.get(`https://openlibrary.org/works/${olid}.json`);
       const result = response.data;
-      bookDescr = result.data.description.value;
+
+   console.log(`Book result: ${JSON.stringify(result)}`);
+
+      let bookDescr = result.data.description.value;
+
+   console.log(`Book descr: ${bookDescr}`);
+
       return bookDescr;
    } catch (err) {
       console.log('Book description not Found');
    }
 }
 
-
-// ========================================================================================//
+// ===================================================================//
 // Display default page - index.ejs
 app.get("/", (req, res) => {
    res.render("index.ejs");
@@ -62,7 +69,16 @@ app.get("/", (req, res) => {
 app.post('/', async (req, res) => {
    let book = req.body.bookTitle.trim();
    let bookData = await getBook(book);
-   console.log(`Book Data: ${bookData}`);
+
+   const bookDesc = await getDescr(bookData.olid);
+
+   // console.log(`Book Data: ${JSON.stringify(bookData)}`);
+   // console.log(`olid: ${bookData.olid}`);
+   // console.log(`Title: ${bookData.title}`);
+   // console.log(`Published: ${bookData.published}`);
+   // console.log(`cover: ${bookData.cover}`);
+
+
    // const bookDescr = await getDescr(bookData.olid);
    // let bookData.descr = bookDescr;
 
