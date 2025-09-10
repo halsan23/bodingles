b84bcffb56dc719c5a83b3093afa5563a3463c7b
halsan23 <halsan23@msn.com>
2025-09-10 11:11:29 -0600

    dd

--- courses/angYu/classWork/9.1_Authentication_Lv.1/index.js
+++ courses/angYu/classWork/9.1_Authentication_Lv.1/index.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // ============================================================================== //
-
+// GET Routes
 app.get("/", (req, res) => {
    res.render("home.ejs");
 });
@@ -22,11 +22,29 @@ app.get("/register", (req, res) => {
    res.render("register.ejs");
 });
 
+// ============================================================================== //
+// POST Routes
+app.post("/register", async (req, res) => {
+   let email = req.body.email.trim();
+   let pswd = req.body.password.trim();
 
-app.post("/register", async (req, res) => {});
+   console.log(`Email: ${email}`);
+   console.log(`Password: ${pswd}`);
+
+   res.redirect(`/`);
+});
 
 
-app.post("/login", async (req, res) => {});
+app.post("/login", async (req, res) => {
+   let email = req.body.email.trim();
+   let pswd = req.body.password.trim();
 
+   console.log(`Email: ${email}`);
+   console.log(`Password: ${pswd}`);
+
+   res.redirect(`/`);
+});
+
+// ============================================================================== //
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
