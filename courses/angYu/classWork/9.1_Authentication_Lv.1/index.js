66604a395bf759ce1e4081aef90f404c22c952b2
halsan23 <halsan23@msn.com>
2025-09-11 11:38:17 -0600

    working on Lvl 1 Authentication badDoggy | aYu

--- courses/angYu/classWork/9.1_Authentication_Lv.1/index.js
+++ courses/angYu/classWork/9.1_Authentication_Lv.1/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import pg from 'pg';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const db = new pg.Client({
+   user: "postgres",
+   password: "password",
+   host: "localhost",
+   database: "secrets",
+   port: 5432,
+});
+db.connect();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -24,25 +34,33 @@ app.get("/register", (req, res) => {
 
 // ============================================================================== //
 // POST Routes
-app.post("/register", async (req, res) => {
+app.post("/login", async (req, res) => {
    let email = req.body.email.trim();
    let pswd = req.body.password.trim();
 
-   console.log(`Email: ${email}`);
-   console.log(`Password: ${pswd}`);
+   // console.log(`Email: ${email}`);
+   // console.log(`Password: ${pswd}`);
 
    res.redirect(`/`);
 });
 
 
-app.post("/login", async (req, res) => {
+app.post("/register", async (req, res) => {
    let email = req.body.email.trim();
    let pswd = req.body.password.trim();
 
-   console.log(`Email: ${email}`);
-   console.log(`Password: ${pswd}`);
+   const chkResult = await db.query("SELECT * FROM users WHERE email = $1", [email]);
 
-   res.redirect(`/`);
+   if ( chkResult.rows.length > 0 ) {
+      res.send('Email already exists, try logging in.');
+   } else {
+      const results = await db.query(
+         'INSERT INTO users (email, pswd) VALUES ($1, $2)',
+         [email, pswd]
+      );
+      console.log(results);
+      res.render("secrets.ejs");
+   }
 });
 
 // ============================================================================== //
