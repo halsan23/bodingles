8512e8f24f3f00dd344f429816a4841685484b67
halsan23 <halsan23@msn.com>
2025-09-24 11:33:46 -0600

    dd

--- temp/temp.js
+++ temp/temp.js
@@ -1,3 +1,8 @@
-let colors = ['Red', 'Green', 'Blue'];
 
-console.log(colors[Math.floor(Math.random() * 3)]);
+
+for (let i = 1; i <= 3; i++) {
+   console.log(i);
+   for (let j = 1; j <= 5; j++) {
+   console.log(`  ${j}`);
+   }
+}
