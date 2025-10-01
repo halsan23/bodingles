9741417e226db89db3b9b603c3c9bcfe2b0b1967
badDoggy <halsan23@msn.com>
2025-10-01 16:53:32 -0600

    finished the documentation for the "Progress Bar" project badDoggy

--- courses/bTraversy/progress_steps_bar/script.js
+++ courses/bTraversy/progress_steps_bar/script.js
@@ -3,42 +3,42 @@ const prev = document.querySelector('#prev')
 const next = document.querySelector('#next')
 const circles = document.querySelectorAll('.circle')
 
-let currActive = 1
+let currActive = 1;
 
 next.addEventListener('click', () => {
-    currActive++
-    if(currActive > circles.length) {
-        currActive = circles.length
-    }
-    update()
+   currActive++
+   if(currActive > circles.length) {
+      currActive = circles.length;
+   }
+   update();
 })
 
 prev.addEventListener('click', () => {
-    currActive--
-    if(currActive < 1) {
-        currActive = 1
-    }
-    update()
+   currActive--
+   if(currActive < 1) {
+      currActive = 1;
+   }
+   update();
 })
 
 function update() {
    circles.forEach((circle, idx) => {
       if(idx < currActive) {
-         circle.classList.add('active')
+         circle.classList.add('active');
       } else {
-         circle.classList.remove('active')
+         circle.classList.remove('active');
       }
    })
 
    const actives = document.querySelectorAll('.active')
-   progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+   progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%';
 
    if (currActive === 1) {
-      prev.disabled = true
+      prev.disabled = true;
    } else if (currActive === circles.length) {
-      next.disabled = true
+      next.disabled = true;
    } else {
-      prev.disabled = false
-      next.disabled = false
+      prev.disabled = false;
+      next.disabled = false;
    }
 }
